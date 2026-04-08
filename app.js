029b96cb331e45ffd67aa0447d3e52e539e14c9e
heloisabarbosanascimento-cloud <heloisa.barbosa.nascimento@escola.pr.gov.br>
2026-04-07 21:00:18 -0300

    v.08

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ let frases = ["Tudo é possível ao que crê ✨",
     "Seja feita a tua vontade, assim na terra como no céu. 🙏"];
 
 let frasesCopiada = [...frase];
-function geradorMensagem(){
-  if(frases.length==0){
-    frases=[...frasesCopiada];
-  }
-  let indiceGerado = Math.floor(Math.random()*(frases.length));
-  let fraseGerada = frases[indiceGerado];
-  console.log(fraseGerada)
-  document.querySelector("#app").textContent=fraseGerada;
-  frases.splice(indiceGerado,1);
+function geradorMensagem() {
+    if (frases.length == 0) {
+        frases = [...frasesCopiada];
+    }
+    let indiceGerado = Math.floor(Math.random() * (frases.length));
+    let fraseGerada = frases[indiceGerado];
+    console.log(fraseGerada)
+    document.querySelector("#msg").textContent = fraseGerada;
 }
 geradorMensagem()

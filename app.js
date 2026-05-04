1c40dffd782a76654688f923c1a72c7352f639a1
heloisabarbosanascimento-cloud <heloisa.barbosa.nascimento@escola.pr.gov.br>
2026-05-03 23:17:30 -0300

    v.11

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let frases = ["Tudo é possível ao que crê ✨",
-    "O amor é paciente, o amor é bondoso. 💕",
-    "Seja feita a tua vontade, assim na terra como no céu. 🙏"];
+let frases = ["A alegria do Senhor é a nossa força",
+    "Que o Deus da esperança os encha de toda alegria e paz, por sua confiança nele.",
+    "Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar.",
+    "Que o Senhor guie seus passos hoje!","Levante-se com alegria e viva a plenitude de Deus.",
+    "Cada manhã é uma nova chance que Deus te dá"];
 
 let frasesCopiada = [...frase];
 function geradorMensagem() {
